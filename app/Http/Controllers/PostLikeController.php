75763b92d1977d4896a142365582273555b06348
Hierro Nabit <roninabit@gmail.com>
2021-01-26 23:28:36 +0700

    initial commit

---
+++ app/Http/Controllers/PostLikeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Mail\PostLiked;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+class PostLikeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+    
+    public function store(Post $post, Request $request)
+    {
+        if ($post->likedBy($request->user())) {
+            return response(null, 409);
+        }
+
+        $post->likes()->create([
+            'user_id' => $request->user()->id,
+        ]);
+
+        Log::info($post->user->email);
+
+        if (!$post->likes()->onlyTrashed()->where('user_id', $request->user()->id)->count()) {
+
+            Mail::to($post->user->email)->send(new PostLiked(auth()->user(), $post));
+        }
+
+        return back();
+    }
+
+    public function destroy(Post $post, Request $request)
+    {
+        $request->user()->likes()->where('post_id', $post->id)->delete();
+
+        return back();
+    }
+}
